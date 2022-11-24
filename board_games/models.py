8125e12870d1bdf2ffd2da2cbec3a96aa6184411
riinahahko <114108520+riinahahko@users.noreply.github.com>
2022-11-24 17:41:32 +0200

    git push

--- board_games/models.py
+++ board_games/models.py
@@ -3,12 +3,11 @@ from django.db import models
 class Game(models.Model):
     """A games the gamers own currently"""
     name = models.CharField(max_length=200)
-    owner = models.CharField(mac_length=200)
+    game_owner = models.TextField(max_length=200)
 
     def __str__(self):
         """Return a string representation of the model."""
         return self.name
-
 class Gamers(models.Model):
     """Board game players that can loan the games."""
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
