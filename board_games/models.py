9c22215b8a7a98569a1b1740603a8c69c9b40444
riinahahko <114108520+riinahahko@users.noreply.github.com>
2022-11-24 16:49:52 +0200

    added a new model Gamers

--- board_games/models.py
+++ board_games/models.py
@@ -7,3 +7,14 @@ class Game(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.name
+
+class Gamers(models.Model):
+    """Board game players that can loan the games."""
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player_name = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'gamers'
+    
+    def __str__(self):
+        return f"{self.game[:50]}..."
