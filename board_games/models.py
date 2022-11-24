1638f59ef387465dee99adc61e0703b008fe6b3a
riinahahko <114108520+riinahahko@users.noreply.github.com>
2022-11-24 17:43:16 +0200

    git push

--- board_games/models.py
+++ board_games/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Game(models.Model):
     """A games the gamers own currently"""
     name = models.CharField(max_length=200)
-    game_owner = models.TextField(max_length=200)
 
     def __str__(self):
         """Return a string representation of the model."""
