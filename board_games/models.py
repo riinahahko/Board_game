411c4b2004d10d622dbc6e1aaff22a2d4ba083f4
riinahahko <114108520+riinahahko@users.noreply.github.com>
2022-11-24 17:23:17 +0200

    adding an owner part to the Game

--- board_games/models.py
+++ board_games/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Game(models.Model):
     """A games the gamers own currently"""
     name = models.CharField(max_length=200)
+    owner = models.CharField(mac_length=200)
 
     def __str__(self):
         """Return a string representation of the model."""
