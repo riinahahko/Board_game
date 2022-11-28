d18d768e046679cd2f5df98f8e8d79f10a6efbbf
riinahahko <114108520+riinahahko@users.noreply.github.com>
2022-11-28 17:30:07 +0200

    Topics view added

--- board_games/views.py
+++ board_games/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Game
 
 def index(request):
     """The home page Board game."""
     return render(request, 'board_games/index.html')
-    
+
+def games(request):
+    """Show all games."""
+    games = Game.objects.order_by('date_added')
+    context = {'games':games}
+    return render( request, 'board_games/games.html', context)
+
