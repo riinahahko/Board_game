bac3555eb45e01b9a050618683d1bbae10e34d8d
riinahahko <114108520+riinahahko@users.noreply.github.com>
2022-11-28 17:55:52 +0200

    updated views.py

--- board_games/views.py
+++ board_games/views.py
@@ -5,9 +5,10 @@ def index(request):
     """The home page Board game."""
     return render(request, 'board_games/index.html')
 
-def games(request):
+def game(request, game_id):
     """Show all games."""
-    games = Game.objects.order_by('date_added')
-    context = {'games':games}
+    game = Game.objects.get(id=game_id)
+    gamers = Game.objects.order_by('date_added')
+    context = {'game':game, 'gamers':gamers}
     return render( request, 'board_games/games.html', context)
 
