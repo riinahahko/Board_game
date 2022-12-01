efaf19efa2aa9172f6bebbaea0d7b8a127deb878
Dominzki <tomi.ahonen@edu.turkuamk.fi>
2022-12-01 10:30:12 +0200

    new_game, forms ja games päivitetty

--- board_games/forms.py
+++ board_games/forms.py
@@ -1,10 +1,9 @@
 from django import forms
+from .models import Game, Gamers
 
-from .models import Game
 
 class GameForm(forms.ModelForm):
     class Meta:
         model = 'Game'
         fields = ['name']
         labels = {'name':''}
-        
